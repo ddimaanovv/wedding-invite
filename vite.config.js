94a0a8fe5b5a4854ab3f573968ded884d3596912
ddimaanovv <slam071196@gmail.com>
2026-02-25 00:34:28 +0300

    fix gh actions

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true'
+const repoBase = '/wedding-invite/'
+
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
+  base: isGithubActions ? repoBase : '/',
   plugins: [react()],
   server: {
     host: true,
